3adaffd2558f37d4fdac54a8c0b1d7e584c8d9f0
Agung Tata Fahreza <fahreza.atf@gmail.com>
2026-01-11 00:59:51 +0700

    m

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,6 @@
 
 use App\Events\TestEvent;
 use App\Http\Controllers\admin\ApplicantController;
-use App\Http\Controllers\admin\AttendanceController;
-use App\Http\Controllers\admin\BehaviorAssessmentController;
-use App\Http\Controllers\admin\BehaviorScoreController;
-use App\Http\Controllers\admin\ChatController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\admin\DashboardController;
 use App\Http\Controllers\admin\MedicalFormController;
@@ -48,31 +44,6 @@ Route::group(['middleware' => ['auth']], function () {
         Route::get('/', [DashboardController::class, 'index']);
         Route::get('/dashboard', [DashboardController::class, 'index']);
         Route::post('/dashboard/summary', [DashboardController::class, 'summary']);
-
-        // Route::get('/menu', [MenuController::class, 'index'])->middleware('access:Menu,view');
-        // Route::post('/menu/show', [MenuController::class, 'show'])->middleware('access:Menu,view');
-        // Route::post('/menu/add', [MenuController::class, 'add'])->middleware('access:Menu,add');
-        // Route::post('/menu/edit', [MenuController::class, 'edit'])->middleware('access:Menu,edit');
-        // Route::post('/menu/update', [MenuController::class, 'update'])->middleware('access:Menu,edit');
-        // Route::post('/menu/delete', [MenuController::class, 'delete'])->middleware('access:Menu,delete');
-
-        // Route::get('/role', [RoleController::class, 'index'])->middleware('access:Role,view');
-        // Route::post('/role/show', [RoleController::class, 'show'])->middleware('access:Role,view');
-        // Route::get('/role/add', [RoleController::class, 'add'])->middleware('access:Role,add');
-        // Route::post('/role/create', [RoleController::class, 'create'])->middleware('access:Role,add');
-        // Route::get('/role/edit/{role_id}', [RoleController::class, 'edit'])->middleware('access:Role,edit');
-        // Route::post('/role/update', [RoleController::class, 'update'])->middleware('access:Role,edit');
-        // Route::post('/role/delete', [RoleController::class, 'delete'])->middleware('access:Role,delete');
-
-        // Route::get('/user', [UserController::class, 'index'])->middleware('access:User,view');
-        // Route::post('/user/show', [UserController::class, 'show'])->middleware('access:User,view');
-        // Route::post('/user/add', [UserController::class, 'add'])->middleware('access:User,add');
-        // Route::post('/user/edit', [UserController::class, 'edit'])->middleware('access:User,edit');
-        // Route::post('/user/update', [UserController::class, 'update'])->middleware('access:User,edit');
-        // Route::post('/user/delete', [UserController::class, 'delete'])->middleware('access:User,delete');
-        // Route::post('/user/reset-password', [UserController::class, 'resetPassword'])->middleware('access:User,edit');
-        // Route::post('/user/update-password', [UserController::class, 'updatePassword']);
-        // Route::get('/user/get-user', [UserController::class, 'getUsers']);
 
         Route::get('/year', [YearController::class, 'index'])->middleware('access:Tahun,view');
         Route::post('/year/show', [YearController::class, 'show'])->middleware('access:Tahun,view');
